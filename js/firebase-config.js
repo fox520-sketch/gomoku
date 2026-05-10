c42eb479c6702413e73b1473810f48ad73b32129
fox520-sketch <fox520486@gmail.com>
2026-05-10 10:38:23 +0800

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,7 @@
 // 使用線上雙人模式前，請將這個檔案內容改成你的 Firebase Web App 設定。
 // 也可以參考 firebase-config.example.js。
 export const firebaseConfig = {
-  apiKey: "AIzaSyCN8EnOOb0tS8A5JjJSeHDrndrSBK-n8k8",
+  apiKey: "AIzaSyCN8En0Ob0tS8A5JjJSeHDrndrSBK-n8k8",
   authDomain: "gomoku-b8c53.firebaseapp.com",
   projectId: "gomoku-b8c53",
   storageBucket: "gomoku-b8c53.firebasestorage.app",
